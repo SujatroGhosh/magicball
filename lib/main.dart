8ae9491d62325243f133a0fb26cd0f40f4d26939
Sujatro Ghosh <sujatroghosh@Sujatros-MacBook-Air.local>
2022-07-19 15:16:43 +0530

    First

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Magic 8 Ball'),
+          backgroundColor: Colors.blue.shade500,
+        ),
+        body: MagicBall(),
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int MagicNumber = 1;
+  void changeface() {
+    setState(() {
+      MagicNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeface();
+              },
+              child: Image.asset('images/ball$MagicNumber.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
